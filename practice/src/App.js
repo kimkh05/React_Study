5891cba8e0dcb0b74f3591acad012d6dfdf40b27
김경호 <nestjs01@naver.com>
2021-10-19 22:49:29 +0900

    React : props 를 통해 컴포넌트에게 값 전달하기

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 import Hello from './Hello';
 
 function App() {
-  const name = 'react';
-  const style = {
-    backgroundColor: 'black',
-    color: 'aqua',
-    fontSize: 24,
-    padding: '1rem'
-  }
   return(
     <>
-      {/* 주석은 화면에 보이지 않습니다 */}
-      /* 중괄호로 감싸지 않으면 화면에 보입니다 */
-      <Hello 
-      />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
+      <Hello name="react" color="red" />
+      <Hello coor="pink" />
     </>
   );
 }
--- practice/src/Hello.js
+++ practice/src/Hello.js
@@ -1,5 +1,9 @@
 import React from 'react';
-function Hello(){
-    return <div>Hello world</div>
+function Hello(props){
+    return <div style={{ color: props.color}} >안녕하세요 {props.name}</div>
 }
+Hello.defaultProps = {
+    name: '이름없음'
+}
+
 export default Hello;
