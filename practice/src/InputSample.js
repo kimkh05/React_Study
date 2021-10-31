8f81992fea9d7aef77bf27119047d795e2a2046b
김경호 <nestjs01@naver.com>
2021-10-31 21:10:20 +0900

    React : 여러개의 input 상태 관리하기

--- practice/src/InputSample.js
+++ practice/src/InputSample.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
-function InputSample() {
+function InputSample(){
     const [inputs, setInputs] = useState({
         name: '',
         nickname: ''
     });
-    
-    const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
-
-    const onChange = (e) => {
+    const nameInput = useRef();
+    const { name, nickname } = inputs;
+    const onChange = e => {
         const { value, name } = e.target;
         setInputs({
             ...inputs,
@@ -17,18 +16,30 @@ function InputSample() {
     };
     const onReset = () => {
         setInputs({
-            name: '',
-            nickname: '',
-        })
+            name: '', 
+            nickname: ''
+        });
+        nameInput.current.focus();
     };
 
     return(
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
-            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
+            <input
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value = {name}
+                ref={nameInput}
+            />
+            <input
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}
+            />
             <button onClick={onReset}>초기화</button>
             <div>
-                <b>값: </b>
+                <b>값 : </b>
                 {name} ({nickname})
             </div>
         </div>
