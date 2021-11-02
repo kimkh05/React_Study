605a273c618a2cbc544fe09da3571770bd3df20a
김경호 <nestjs01@naver.com>
2021-11-02 19:06:10 +0900

    React : Todolist 만들어 보기

---
+++ todolist/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <div>안녕하세요</div>
+    </>
+  );
+}
+
+export default App;
