c956f6bd82d65a1209d746e97f72e87321230611
김경호 <nestjs01@naver.com>
2021-11-03 22:48:15 +0900

    React : useRef 로 컴포넌트 안의 변수 만들기

--- practice/src/UserList.js
+++ practice/src/UserList.js
@@ -1,36 +1,21 @@
 import React from 'react';
 
-function UserList(){
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email : 'liz@example.com'
-        }
-    ];
-    return(
-        <div>
-            <div>
-                <b>{users[0].username}</b> <span>({users[0].email})</span>
-            </div>
-            <div>
-                <b>{users[1].username}</b> <span>({users[1].email})</span>
-            </div>
-            <div>
-                <b>{users[2].username}</b> <span>({users[2].email})</span>
-            </div>
-        </div>
-    );
+function User({ user }) {
+  return (
+    <div>
+      <b>{user.username}</b> <span>({user.email})</span>
+    </div>
+  );
+}
+
+function UserList({ users }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
 }
 
 export default UserList;
