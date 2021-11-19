d4bff7a388aba8748ab7f2cd645357838c838219
김경호 <nestjs01@naver.com>
2021-11-19 19:06:35 +0900

    ⚙️파일 : Github users information API Practice

--- cho/src/components/practiceApi/ApiPractice.js
+++ cho/src/components/practiceApi/ApiPractice.js
@@ -9,18 +9,20 @@ const ApiPractice = () => {
       url: "https://api.github.com/users/kimkh05",
     })
       .then((res) => {
-        console.log(res);
         setData(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
+
   return (
     <>
-    <h1>API 연동 연습</h1>
+      <h1>API 연동 연습</h1>
       <div>Here is My gitHub followers_url : {data.followers_url}</div>
       <div>Here is My company : {data.company}</div>
+      <div>Here is My name : {data.name}</div>
+      <div>Here is My bio : {data.bio}</div>
     </>
   );
 };
